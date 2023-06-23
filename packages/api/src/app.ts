d4bcf55c194c33d9f4633c382e458fdc7f4f376a
Evan Fleet <evfleet@gmail.com>
2023-06-23 12:56:57 -0400

    feat(api): :loud_sound: setup logging for api

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import pino from "pino-http";
+import logger from "./loaders/logger";
 
 const build = () => {
   const app = express();
+
+  app.use(pino({ logger }));
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import logger from "./loaders/logger";
 
 const port = process.env.PORT || 8080;
 
@@ -6,7 +7,7 @@ const start = async () => {
   const server = await app.build();
 
   server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    logger.info(`Listening on port ${port}`);
   });
 };
 
