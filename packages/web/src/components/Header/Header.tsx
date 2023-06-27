9360d80e931ce4efc5e2a1c7c53be5e1974c6c76
Evan Fleet <evfleet@gmail.com>
2023-06-26 21:24:43 -0400

    feat(web): :sparkles: setup radix libraries

--- packages/web/src/components/Header/Header.tsx
+++ packages/web/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="container mx-auto border-b-2 px-4 py-2">
+    <header className="container mx-auto border-b-2 bg-gray-10 px-4 py-2">
       <span>Bosun</span>
     </header>
   );
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: generateScale("gray"),
+      },
+    },
   },
   plugins: [],
 };
+
+// Inspired from https://fynn.at/shorts/2023-03-19-how-to-use-radix-colors-with-tailwind-css
+// Fixed breaking changes from 1.0.0 release
+function generateScale(name) {
+  let scale = Array.from({ length: 12 }, (_, i) => {
+    let id = i + 1;
+    return [
+      [id, `var(--${name}-${id})`],
+      [`a${id}`, `var(--${name}-a${id})`],
+    ];
+  }).flat();
+
+  return Object.fromEntries(scale);
+}
