7220a869e4a6c3b944a7e588051b3ab99c6711ba
Evan Fleet <evfleet@gmail.com>
2023-06-28 16:30:30 -0400

    refactor(web): :zap: refactor to nextjs

--- packages/web/src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="container mx-auto px-4 py-2">
-      <p>Footer</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- packages/web/src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="container mx-auto border-b-2 bg-gray-10 px-4 py-2">
-      <span>Bosun</span>
-    </header>
-  );
-};
-
-export default Header;
