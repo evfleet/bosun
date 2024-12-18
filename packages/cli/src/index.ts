7ab4f53d7bdbae3d5d269005ffa81ce928c82c2e
Evan Fleet <evfleet@gmail.com>
2024-12-18 16:56:31 -0500

    feat(cli): setup basic cli structure

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,39 @@
 #! /usr/bin/env node
+import { confirm } from "@inquirer/prompts";
+import { Command } from "commander";
 
-console.log("Hello from Bosun");
+const program = new Command();
+
+program
+  .name("bosun")
+  .description("CLI to publish and transform images")
+  .version("0.1.0");
+
+program.action(() => {
+  console.log("Bosun!");
+});
+
+program
+  .command("init")
+  .description("initialize")
+  .option("-y, --yes", "confirm all")
+  .action(async (options, command) => {
+    if (options.yes) {
+      console.log("initializing with skip");
+      return;
+    }
+
+    const question = await confirm({ message: "Question?" });
+
+    console.log("question", question);
+    console.log("initializing bosun");
+  });
+
+program
+  .command("publish <input>")
+  .description("upload image to server")
+  .action((input) => {
+    console.log(`this is the input ${input}`);
+  });
+
+program.parse(process.argv);
