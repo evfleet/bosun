fc5581d11781ea676289b179d63e6e23cd074112
Evan Fleet <evfleet@gmail.com>
2024-12-19 00:57:49 -0500

    feat(api): setup basic endpoint to accept file upload

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,7 +1,34 @@
+import busboy from "busboy";
 import express from "express";
 
 export function build() {
   const app = express();
+
+  app.post("/upload", (req, res, next) => {
+    try {
+      const bb = busboy({
+        headers: req.headers,
+      });
+
+      bb.on("error", (err) => {
+        next(err);
+      });
+
+      bb.on("file", (name, file) => {
+        console.log("file", name);
+      });
+
+      bb.on("close", () => {
+        bb.removeAllListeners();
+      });
+
+      req.pipe(bb);
+    } catch (err) {
+      res.status(400).json({
+        message: `Could not retrieve file from request`,
+      });
+    }
+  });
 
   return app;
 }
