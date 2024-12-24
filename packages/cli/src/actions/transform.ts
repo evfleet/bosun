812d5a744cb11ef88805f025905c2552bd25d56c
Evan Fleet <evfleet@gmail.com>
2024-12-23 19:18:50 -0500

    refactor(cli): refactor transform into using streams

--- packages/cli/src/actions/transform.ts
+++ packages/cli/src/actions/transform.ts
@@ -1,14 +1,10 @@
-import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 export async function transform(input: string) {
-  const inputPath = path.resolve(process.cwd(), input);
-  const file = await fs.readFile(inputPath);
-  const filename = inputPath.split("/").at(-1);
-
   const form = new FormData();
 
-  form.append("file", new Blob([file]), filename);
+  form.append("file", fs.createReadStream(path.resolve(process.cwd(), input)));
 
   const res = await fetch(`${process.env.BACKEND_URL}/upload`, {
     method: "POST",
